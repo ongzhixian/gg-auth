901aab057fe84e436f03df6b1fc971cc6dce58e0
zong <zong@globalblue.com>
2020-06-17 12:41:05 +0800

    Add project files.

---
+++ GGAuth.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GGAuth.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
